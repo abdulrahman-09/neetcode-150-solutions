9e00cfeff09080e93a213ad8cd909bf5cfb99eb8
abdulrahman-09 <abdulrahman.mujahid09@gmail.com>
2026-07-30 19:16:06 +0300

    solve: valid sudoku

---
+++ src/arrayshashing/ValidSudokuSolution.java
@@ -0,0 +1,59 @@
+package arrayshashing;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class ValidSudokuSolution {
+    public boolean isValidSudoku(char[][] board) {
+        // rows
+        for (int i = 0; i < 9; i++) {
+            Set<Character> existingNums = new HashSet<>();
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    if (existingNums.contains(board[i][j])) {
+                        return false;
+                    }
+                    existingNums.add(board[i][j]);
+                }
+            }
+        }
+        // columns
+        for (int i = 0; i < 9; i++) {
+            Set<Character> existingNums = new HashSet<>();
+            for (int j = 0; j < 9; j++) {
+                if (board[j][i] != '.') {
+                    if (existingNums.contains(board[j][i])) {
+                        return false;
+                    }
+                    existingNums.add(board[j][i]);
+                }
+            }
+        }
+
+        // subboard
+        for (int i = 0; i < 9; i += 3) {
+            for (int j = 0; j < 9; j += 3) {
+                if (!isSubBoardValid(i, j, board))
+                    return false;
+
+            }
+        }
+        return true;
+
+    }
+    private boolean isSubBoardValid(int startRow, int startColumn, char[][] board){
+        Set<Character> existingNums = new HashSet<>();
+        for (int i = startRow; i < startRow + 3; i++) {
+            for (int j = startColumn; j < startColumn + 3; j++) {
+                if (board[i][j] != '.') {
+                    if (existingNums.contains(board[i][j])) {
+                        return false;
+                    }
+                    existingNums.add(board[i][j]);
+                }
+            }
+        }
+        return true;
+    }
+}
