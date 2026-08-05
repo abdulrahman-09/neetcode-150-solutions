f718d97f0dbf21cc1886a775b0fd2771ee857f13
abdulrahman-09 <abdulrahman.mujahid09@gmail.com>
2026-08-05 04:02:07 +0300

    solve: valid palindrome problem

---
+++ src/twopointers/ValidPalindromeSolution.java
@@ -0,0 +1,24 @@
+package twopointers;
+
+public class ValidPalindromeSolution {
+    public boolean isPalindrome(String s) {
+        int start = 0, end = s.length() - 1;
+        while (start < end){
+            while (!Character.isLetterOrDigit(s.charAt(start))){
+                start++;
+                if (start >= end)
+                    return true;
+            }
+            while (!Character.isLetterOrDigit(s.charAt(end))){
+                end--;
+                if (start >= end)
+                    return true;
+            }
+            if (Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end)))
+                return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
